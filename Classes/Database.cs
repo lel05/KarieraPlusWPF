9fd317054f196483a54b29ec81ddd46a25f0f254
lel05 <graczkuba2005@gmail.com>
2024-01-03 20:36:00 +0100

    Naprawienie błędów

--- Classes/Database.cs
+++ Classes/Database.cs
@@ -9,6 +9,7 @@ using System.IO;
 using System.Windows.Markup;
 using System.Windows.Media.Animation;
 using System.Windows;
+using System.Xml.Linq;
 
 namespace KarieraPlus.Classes
 {
@@ -26,11 +27,12 @@ namespace KarieraPlus.Classes
                     raw.SetProvider(new SQLite3Provider_e_sqlite3());
 
                     db.Open();
+
                     var categoryTableCommand = new SqliteCommand();
                     categoryTableCommand.Connection = db;
                     categoryTableCommand.CommandText = "CREATE TABLE `category` (" +
-                                                    "`category_id` PRIMARY KEY AUTOINCREMENT, " +
-                                                    "`name` varchar(50));";
+                                                        "`category_id` INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                                        "`name` varchar(50));";
                     categoryTableCommand.ExecuteReader();
                     db.Close();
 
@@ -38,11 +40,11 @@ namespace KarieraPlus.Classes
                     var companyTableCommand = new SqliteCommand();
                     companyTableCommand.Connection = db;
                     companyTableCommand.CommandText = "CREATE TABLE `company` (" +
-                                                    "`company_id` PRIMARY KEY AUTOINCREMENT, " +
-                                                    "`name` varchar(50)," +
-                                                    "`localization` text," +
-                                                    "`logo` text," +
-                                                    "`description` text);";
+                                                        "`company_id` INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                                        "`name` varchar(50)," +
+                                                        "`localization` text," +
+                                                        "`logo` text," +
+                                                        "`description` text);";
                     companyTableCommand.ExecuteReader();
                     db.Close();
 
@@ -50,7 +52,7 @@ namespace KarieraPlus.Classes
                     var offerTableCommand = new SqliteCommand();
                     offerTableCommand.Connection = db;
                     offerTableCommand.CommandText = "CREATE TABLE `offer` (" +
-                                                    "`offer_id` PRIMARY KEY AUTOINCREMENT," +
+                                                    "`offer_id` INTEGER PRIMARY KEY AUTOINCREMENT," +
                                                     "`company_id` int(10)," +
                                                     "`profession_name` varchar(40)," +
                                                     "`type_of_contract` varchar(50)," +
@@ -58,7 +60,7 @@ namespace KarieraPlus.Classes
                                                     "`salary` text," +
                                                     "`category_id` int(10)," +
                                                     "`duties` text," +
-                                                    "`requirements` text);" +
+                                                    "`requirements` text);";
                     offerTableCommand.ExecuteReader();
                     db.Close();
 
@@ -66,7 +68,7 @@ namespace KarieraPlus.Classes
                     var userTableCommand = new SqliteCommand();
                     userTableCommand.Connection = db;
                     userTableCommand.CommandText = "CREATE TABLE `user` (" +
-                                                    "`user_id` PRIMARY KEY AUTOINCREMENT," +
+                                                    "`user_id` INTEGER PRIMARY KEY AUTOINCREMENT," +
                                                     "`firstname` varchar(30) NOT NULL," +
                                                     "`surname` varchar(30) NOT NULL," +
                                                     "`birth_date` date," +
@@ -90,10 +92,44 @@ namespace KarieraPlus.Classes
                     var offersCalimedTableCommand = new SqliteCommand();
                     offersCalimedTableCommand.Connection = db;
                     offersCalimedTableCommand.CommandText = "CREATE TABLE `offers_claimed` (" +
-                                                    "`offer_id` INTEGER, " +
-                                                    "`user_id` INTEGER;";
+                                                            "`offer_id` INTEGER, " +
+                                                            "`user_id` INTEGER);";
                     offersCalimedTableCommand.ExecuteReader();
                     db.Close();
+
+                    db.Open();
+                    var insertCommand = new SqliteCommand();
+                    insertCommand.Connection = db;
+                    insertCommand.CommandText = "INSERT INTO user (firstname,surname,email,password)" +
+                                                "VALUES (@name,@surname,@email,@password);";
+                    insertCommand.Parameters.AddWithValue("@name", "admin");
+                    insertCommand.Parameters.AddWithValue("@surname", "admin");
+                    insertCommand.Parameters.AddWithValue("@email", "admin@admin.admin");
+                    insertCommand.Parameters.AddWithValue("@password", "admin");
+                    insertCommand.ExecuteNonQuery();
+                    db.Close();
+
+                    db.Open();
+                    var insertCompanyCommand = new SqliteCommand();
+                    insertCompanyCommand.Connection = db;
+                    insertCompanyCommand.CommandText = "INSERT INTO company (name, localization, logo, description)" +
+                                                "VALUES (@name,@localization,@logo,@description);";
+                    insertCompanyCommand.Parameters.AddWithValue("@name", "Idel");
+                    insertCompanyCommand.Parameters.AddWithValue("@localization", "Limanowa");
+                    insertCompanyCommand.Parameters.AddWithValue("@logo", "Idel_logo.png");
+                    insertCompanyCommand.Parameters.AddWithValue("@description", "firma IT");
+                    insertCompanyCommand.ExecuteNonQuery();
+                    db.Close();
+
+                    db.Open();
+                    var insertCategoryCommand = new SqliteCommand();
+                    insertCategoryCommand.Connection = db;
+                    insertCategoryCommand.CommandText = "INSERT INTO category (name)" +
+                                                "VALUES (@name);";
+                    insertCategoryCommand.Parameters.AddWithValue("@name", "IT");
+                    insertCategoryCommand.ExecuteNonQuery();
+                    db.Close();
+
                 }
             }
         }
